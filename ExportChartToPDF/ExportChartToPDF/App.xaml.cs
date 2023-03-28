5708e8defb9c0fb24ca93cea5ff325812eaeff6d
VimalaThirumalaiKumarSF3739 <vimala.t@syncfusion.com>
2023-03-28 19:12:23 +0530

    Sample update

---
+++ ExportChartToPDF/ExportChartToPDF/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace ExportChartToPDF;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ ExportChartToPDF/ExportChartToPDF/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace ExportChartToPDF;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
